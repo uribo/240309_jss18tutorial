ce0807352b0034aea44441124677951d8b0d9cfe
Shinya Uryu <suika1127@gmail.com>
2024-03-01 17:47:13 +0900

    [add]リポジトリのQRコードを生成

---
+++ misc/qrcode.R
@@ -0,0 +1,10 @@
+library(ragg)
+library(qrcode)
+x <-
+  qr_code("https://github.com/uribo/240309_jss18tutorial")
+
+png("images/qrcode.png", width = 200, height = 200, res = 96)
+plot(x)
+dev.off()
+
+
