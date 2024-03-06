e37446f3da8f61a42aa800639f11d4bde9d66c49
Shinya Uryu <suika1127@gmail.com>
2024-03-06 10:29:17 +0900

    [modify]tweak

--- part1/00setup.R
+++ part1/00setup.R
@@ -1,5 +1,4 @@
 # tidymodels --------------------------------------------------------------
-library(tidymodels)
 library(ranger)
 # library(censored) # 生存時間解析のためのparsnipの拡張（扱わない）
 # library(poissonreg)
@@ -13,22 +12,5 @@ library(bonsai) # 木モデルのためのparsnipの拡張
 # library(themis)
 
 # DALEX -------------------------------------------------------------------
-library(DALEX)
 library(ggpubr)
-# install_dependencies()
-apartments_lm <- lm(m2.price ~ ., data = apartments)
-anova(apartments_lm)
-
-apartments_test$m2.price[1:6]
-predict(apartments_lm, apartments_test[1:6,])
-
-predicted_apartments_lm <- predict(apartments_lm, apartments_test)
-sqrt(mean((predicted_apartments_lm - apartments_test$m2.price)^2))
-
-apartments_lm_exp <- explain(model = apartments_lm,
-                             data = apartments_test[,-1],
-                             y = apartments_test$m2.price,
-                             label = "Linear Regression")
-
-# x <- predict_parts(...)
-# plot(x)
+# DALEX::install_dependencies()
